6ab13b93cdd9631844ced57b0cde7dfc05502feb
ted <linyc1983@gmail.com>
2015-05-14 16:16:04 +0800

    tuple example

--- your_python_script.py
+++ your_python_script.py
@@ -50,3 +50,38 @@ thing = Negator()
 print thing == None
 
 print thing is None
+
+if name in ('Alice', 'Bob', 'Charlie'):
+    print 'ok'
+
+if name == 'Alice' or name == 'Bob' or name == 'Charlie':
+    print 'ok
+
+msg = 'Hello world'
+if 'world' in msg:
+    print 'find it'
+
+
+msg = 'Hello world'
+if msg.find('world') != -1:
+    print 'find it'
+
+
+t = 12345, 54321, 'hello!'
+x, y, z = t
+
+my_list = ['Alice', 12, 'Python']
+(name, age, skill) = my_list
+
+
+my_list = ['Alice', 12, 'Python']
+name = my_list[0]
+age = my_list[1]
+skill = my_list[2]
+
+(x, y) = (y, x)
+
+def get_error_details():
+    return (2, 'details')
+
+(errnum, errstr) = get_error_details()
